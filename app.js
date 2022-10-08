ece300d8bbf354c60e1d59303fc76835143b32d9
DenverCoder2009 <76412568+DenverCoder2009@users.noreply.github.com>
2022-10-08 15:17:22 -0700

    Create new application API

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("./app_api/database/db");
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
 var { hasSubscribers } = require('diagnostics_channel');
 //var { handlebars } = require('hbs');
 var hbs = require('hbs');
+//require(".app_api/database/db");
 
 var app = express();
 
@@ -36,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
