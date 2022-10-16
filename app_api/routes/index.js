a5286beb81b8f84fdfc509f63020cd1d314437eb
DenverCoder2009 <76412568+DenverCoder2009@users.noreply.github.com>
2022-10-15 17:56:44 -0700

    Module6 par 5

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,7 +5,8 @@ var tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
